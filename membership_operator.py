b849d73af79eebd67aa99bc78a6c1fad5673a315
pavanmandalapalli <pavanmandalapalli@gmail.com>
2026-07-11 20:38:45 +0530

    Add files via upload

---
+++ membership_operator.py
@@ -0,0 +1,18 @@
+
+# in, and , or
+
+
+s = "abcdef"
+
+if 'a' in s:
+    print('a prsent in s')
+    
+if 'g' in s:
+    print('g present in s')
+else:
+    print('g is not present')
+
+if 'a' and 'b':
+    print(True)
+if 'a' or 'z':
+    print(True)
