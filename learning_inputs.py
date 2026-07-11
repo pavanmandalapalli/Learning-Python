7ce7e280a10f0634dfce58e1e85c3fcea0f0925f
pavanmandalapalli <pavanmandalapalli@gmail.com>
2026-07-11 20:11:03 +0530

    Add files via upload

---
+++ learning_inputs.py
@@ -0,0 +1,3 @@
+string = input("This is a String input :")
+number = int(input("This is a Number INput: ")) # "1" -> 1
+boolean = bool(input("This is a boolean Input: ")) # true - > True ,# false -> False
